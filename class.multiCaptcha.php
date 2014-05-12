28ae9b68c3368852272b8db7a6b966fc643ef476
sameer-shelavale <samiirds@gmail.com>
2014-05-12 22:57:01 +0530

    Skeleton of the base class

---
+++ class.multiCaptcha.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by Sameer Shelavale.
+ * User: sameer-shelavale
+ * Date: 5/12/14
+ * Time: 8:26 PM
+ */
+
+class MultiCaptcha {
+
+    var $secretKey = "{{((o=All-Hands-More-Sail=o))}}"; //you MUST change this in live environment
+
+    var $supportedTypes = array(
+        'recaptcha',    //captcha by google
+        'image',        //image captcha
+        'honeypot',     //honeypot, mainly for spambots, adds an hidden field which only the spambots fill in
+        'checkbox',     //adds an checkbox which is supposed to be left blank by humans, but bots mark it checked
+        'math',         //Math captcha which asks answer of simple mathematical questions
+        'ascii',        //ASCII captcha, displays the captcha in ASCII decorative style
+        'animated',     //GIF animated captcha
+    );
+
+    var $selectedTypes = array('recaptcha');    //multiple types can also be specified,
+                                                //in that case the captcha will be randomized from the selected types
+
+
+    /*
+     * function captcha()
+     * @return html of the captcha code, this is to be inserted in the forms directly
+     */
+    public function captcha(){
+
+    }
+
+
+    /*
+     * function validate()
+     * @param $fieldName name of the captcha field
+     * @param $fieldValue value of the captcha field as typed by the user
+     * @return boolean
+     */
+    public function validate( $fieldName, $fieldValue ){
+
+        return false;
+    }
+
+
+    /*
+     * function validate()
+     * @param $data posted data
+     * @return string field name for the captcha
+     */
+    public function getCaptchaField( $data ){
+
+    }
+
+}
