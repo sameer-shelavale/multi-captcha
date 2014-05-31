166290f8c17c4db2bbb7d4bbe8b53edc320b9747
sameer-shelavale <samiirds@gmail.com>
2014-05-31 23:29:03 +0530

    added example

---
+++ test/test1.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sam
+ * Date: 5/22/14
+ * Time: 5:24 PM
+ */ 
+//Public Key: 	6Le1XPQSAAAAAPteoelqqH7JQzKGOikcTc3HrpAA
+//Private Key: 	6Le1XPQSAAAAABu9LOXfj6Wxvec_cQFXCHjOJzG3
+
+include_once( "../class.multiCaptcha.php");
+
+$captcha = new MultiCaptcha(
+    "form1-secret-key",
+    array(
+        'honeypot' => array(
+            'description'=> "Get lost you bot",
+            'class' => 'error'
+        )
+    )
+);
+
+if( isset( $_REQUEST['submit'] ) ){
+    var_dump( $captcha->validateForm( $_POST, $_SERVER['REMOTE_ADDR'] ) );
+}
+?>
+
+<form action="<?php echo $_SERVER['PHPSELF']; ?>" method="post">
+    <?php
+    echo $captcha->getHtml() ;
+?>
+    <input type="submit" name="submit" value="Submit">
+
+</form>
+
