4eb9084d795dae3b8787d62fc904dd3f08a041f8
sameer-shelavale <samiirds@gmail.com>
2014-09-05 14:44:05 +0530

    Updated DefaultTheme to actually display the captcha properly.

--- themes/DefaultTheme.php
+++ themes/DefaultTheme.php
@@ -18,21 +18,92 @@ class DefaultTheme {
 
 
     function render( $data ){
-        return
-            '<div id="MultiCaptchaWrapper">
-                <div id="MultiCaptchaGraphic">
-                    <img src="" alt="" />
-                </div>
-                <div id="MultiCaptchaTools">
+        if( $data['customFieldName'] ){
+            $fieldName = $data['customFieldName'];
+        }else{
+            $fieldName = $data['cipher'];
+        }
+
+        $html =
+            '<div
+                style="border:3px solid #000;
+                    border-radius: 5px;
+                    padding: 10px;
+                    display: table;
+                    margin: 2px;
+                    background-color: #f69d03;
+                    font-family: Arial;
+                    font-size: 14px;
+                    max-width: 200px" >';
+        if( isset( $data['question'] ) && $data['question']['type'] == 'image' ){
+            $html .=
+                '<div>
+                    <img
+                        src="'.$data['question']['url'].'"
+                        alt=""
+                        style="
+                            border-radius:3px;
+                            margin-bottom:5px;
+                        "
+                    />
+                </div>';
+        }
+
+        if( isset( $data['question'] ) && $data['question']['type'] == 'text' ){
+            $html .=
+                '<div
+                    style="font-size:120%;
+                    font-weight:bold;">'
+                .$data['question']['content']
+                .'</div>';
+        }
+
+        if( isset( $data['question'] ) && $data['question']['type'] == 'ascii' ){
+            $html .=
+                '<div
+                    style="
+                        background-color:#ccc;
+                        border-radius:3px;
+                    " >'
+                .$data['question']['content']
+                .'</div>';
+        }
+
+        $html .= '<div>
+                    <input
+                        type="text"
+                        name="'.$fieldName.'"
+                        title="'.$data['tooltip'].'"
+                        style="
+                            background-color:#f66a03;
+                            border:2px solid #fff;
+                            font-size:120%;
+                            font-weight:bold;
+                            border-radius:3px;
+                        "
+                    />';
+
+        if( $data['cipher'] != $fieldName ){
+            $html .= '<input type="hidden" name="'.$fieldName.'_challenge" value="'.$data['cipher'] .'" /> ';
+        }
+        $html .= '</div>';
+
+        $html .= '<div
+                    style="font-size:80%;
+                    line-height:100%;">'.$data['description'].'</div>';
+
+        if( isset( $data['helpUrl'] ) || isset( $data['helpHtml'] ) || isset( $data['refreshUrl'] ) ){
+            $html .= '
+                <div>
                     <a href="" title="Help">test1</a>
                     <a href="" title="Refresh">test2</a>
-                </div>
-                <div id="MultiCaptchaQuestion">
+                </div>';
+        }
 
-                </div>
-                <div id="MultiCaptchaInput">
-                    <input type="text" name="" />
-                </div>
-            </div>';
+
+        $html .= '</div>';
+
+        return $html;
+
     }
 } 
