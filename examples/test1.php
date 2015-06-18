1b7cd65d9bdf2141539abb4d86fc9d3bc6decb1f
Sameer Shelavale <sameer@gohachi.com>
2015-06-18 14:54:15 +0530

    updated the sample code to cater for  updates in the constructor

--- examples/test1.php
+++ examples/test1.php
@@ -9,62 +9,57 @@
 //Private Key: 	6Le1XPQSAAAAABu9LOXfj6Wxvec_cQFXCHjOJzG3
 
 
-include_once( "../class.multiCaptcha.php");
+include_once( "../src/captcha.php");
 
 
-$captcha = new \MultiCaptcha\Captcha(
-    "form1-secret-key",
-    /*array(
+$captcha = new \MultiCaptcha\Captcha([
+    'secret'=>    "form1-secret-key",
+    'options' =>  [
+    /*
         'honeypot' => array(
             'description'=> "Leave this field empty if you are human",
             'class' => 'error'
-        )
-    )
-    array(
+        ),
         'math' => array(
             'description'=> "Answer following question if you are human",
             'level' => 4
-        )
-    )
-    array(
+        ),*/
         'image' => array(
             'maxCodeLength' => 8,
-            'font'=>'../types/image/comic.ttf',
-            'width'=>175
-        )
-    )
-    array(
+            'font'=>'../src/types/image/comic.ttf',
+            'width'=>180
+        )/*
+
+
         'ascii' => array(
             'maxCodeLength' => 8,
-            'fontPath'=>'../types/ascii/fonts/',
+            'fontPath'=>'../src/types/ascii/fonts/',
             'fonts'=>array(
                 'banner'=> 4,
                 'doom'=> 8,
                 'small'=>'8'
             )
-        )
-    )
-    */
-    array(
+        ),
         'gif' => array(
             'maxCodeLength' => 8,
-            'font'=>'../types/image/comic.ttf',
+            'font'=>'../src/types/image/comic.ttf',
             'width'=>180,
             'height'=>60,
             'totalFrames'=>50,
             'delay'=>20
         )
-    )
-);
+*/
+    ]
+] );
 
 if( isset( $_REQUEST['submit'] ) ){
     var_dump( $captcha->validateForm( $_POST, $_SERVER['REMOTE_ADDR'] ) );
 }
 ?>
 
-<form action="<?php echo $_SERVER['PHPSELF']; ?>" method="post">
+<form action="" method="post">
     <?php
-    echo $captcha->getHtml() ;
+    echo $captcha->render() ;
     ?>
     <input type="submit" name="submit" value="Submit">
 
