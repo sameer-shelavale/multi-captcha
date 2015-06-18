44f67e3ca688151b2621a056109a3418e87cea4b
Sameer Shelavale <sameer@gohachi.com>
2015-06-18 15:54:04 +0530

    renamed class to psr-4 standard

---
+++ examples/image-captcha.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once( "../src/captcha.php");
+
+
+$captcha = new \MultiCaptcha\Captcha([
+    'secret'=>    "form1-secret-key",
+    'options' =>  [
+        'image' => [
+            'maxCodeLength' => 8,
+            'font'=>'../src/types/image/comic.ttf',
+            'width'=>180
+        ]
+    ]
+] );
+
+if( isset( $_REQUEST['submit'] ) ){
+    var_dump( $captcha->validateForm( $_POST, $_SERVER['REMOTE_ADDR'] ) );
+}
+?>
+
+<form action="" method="post">
+    <?php
+    echo $captcha->render() ;
+    ?>
+    <input type="submit" name="submit" value="Submit">
+
+</form>
+
