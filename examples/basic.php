1950e15ce58f8c0a0f93774a579962655f693094
Sameer Shelavale <sameer@gohachi.com>
2015-06-19 16:58:36 +0530

    added an example of minimalistic captcha setup

---
+++ examples/basic.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once( "../src/captcha.php");
+
+
+$captcha = new \MultiCaptcha\Captcha([
+    'secret'=>    "form1-secret-key",
+] );
+
+if( isset( $_REQUEST['submit'] ) ){
+    var_dump( $captcha->validateForm( $_POST, $_SERVER['REMOTE_ADDR'] ) );
+}
+?>
+<h2>Display a random captcha from types initialized using "<i>options</i>" parameter.</h2>
+<form action="" method="post">
+    <?php
+    echo $captcha->render() ;
+    ?>
+    <input type="submit" name="submit" value="Submit">
+
+</form>
+
