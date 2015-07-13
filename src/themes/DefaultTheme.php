656f9966a7c2005db9fc1094599f7a726783f411
Sameer Shelavale <sameer@gohachi.com>
2015-07-13 20:40:36 +0530

    added javascript function for refreshing captcha. the render() function is not split over multiple functions so that we can change themes more easily and refreshing complex themes(which are spread over multiple containers e.g. two table td) can be a bit easier.

--- src/themes/DefaultTheme.php
+++ src/themes/DefaultTheme.php
@@ -9,17 +9,38 @@ namespace MultiCaptcha\Themes;
 
 class DefaultTheme {
     var $fieldClass = '';
-    var $fieldStyle = 'background-color:#f66a03; border:2px solid #fff; font-size:120%; font-weight:bold; border-radius:3px;';
-    var $containerStyle = 'border:3px solid #000; border-radius: 5px; padding: 10px; display: table; margin: 2px; background-color: #f69d03; font-family: Arial; font-size: 14px; max-width: 200px';
+    var $fieldStyle = 'background-color:#f66a03; border:2px solid #fff; font-size:120%; font-weight:bold; border-radius:3px;width:144px;';
+    var $containerStyle = 'border:3px solid #000; border-radius: 5px; padding: 10px; display: table; margin: 2px; background-color: #f69d03; font-family: Arial; font-size: 14px; max-width: 180px;position:relative;';
     var $questionImageStyle = 'border-radius:3px; margin-bottom:5px;';
-    var $questionTextStyle = 'font-size:120%; font-weight:bold;';
-    var $questionAsciiStyle = ' background-color:#ccc; border-radius:3px;';
-    var $descriptionStyle = 'font-size:80%; line-height:100%;';
+    var $questionTextStyle = 'font-size:120%; font-weight:bold;background-color:#ccc; border-radius:3px; padding:4px;margin-bottom:2px;text-align:center;display:block;';
+    var $questionAsciiStyle = 'background-color:#ccc; border-radius:3px; padding:4px;margin-bottom:2px;text-align:center;display:block;';
+    var $questionContainerStyle = '';
+    var $labelStyle = 'font-size:80%; line-height:100%;';
 
-    var $helpBtnClass = '';
+    var $helpBtnClass = 'btn-help';
     var $helpBtnText = '?';
-    var $refreshBtnClass = '';
+    var $refreshBtnClass = 'btn-refresh';
     var $refreshBtnText = '&#8634;';
+    var $extraHtml = <<<'EOT'
+<style type="text/css">
+a.btn-refresh, a.btn-help{
+    background-color:#fff;
+    text-decoration:none;
+    color:#f66a03;
+    padding:1px 2px;
+    border-radius:2px;
+    vertical-align:top;
+    margin-left:2px;
+    display:inline-block;
+    width:12px;
+    height:12px;
+    text-align:center;
+    line-height:100%;
+    font-size:12px;
+}
+</style>
+EOT;
+
 
 
     function __construct( $customValues ){
@@ -35,74 +56,103 @@ class DefaultTheme {
         $html =
             '<div
                 style="'.$this->containerStyle.'" >';
-        if( isset( $data['question'] ) && $data['question']['type'] == 'image' ){
-            $html .=
-                '<div>
-                    <img
-                        src="'.$data['question']['url'].'"
-                        alt=""
-                        style="'.$this->questionImageStyle.'"
-                    />
-                </div>';
-        }
 
-        if( isset( $data['question'] ) && $data['question']['type'] == 'text' ){
-            $html .=
-                '<div style="'.$this->questionTextStyle.'">'
-                .$data['question']['content']
-                .'</div>';
-        }
+        $html .= '<div style="'.$this->questionContainerStyle.'">'. $this->renderChallenge( $data ).'</div>';
 
-        if( isset( $data['question'] ) && $data['question']['type'] == 'ascii' ){
-            $html .=
-                '<div style="'.$this->questionAsciiStyle.'" >'
-                .$data['question']['content']
-                .'</div>';
-        }
+        $html .= '<div>'.$this->renderResponseField( $data ).$this->renderTools( $data ).'</div>';
 
-        $html .= '<div>
-                    <input
-                        type="text"
-                        name="'.$data['fieldName'].'"
-                        title="'.$data['tooltip'].'"
-                        style="'.$this->fieldStyle.'"
-                        class="'.$this->fieldClass.'"
-                    />';
+        $html .= '<div>'.$this->renderLabel( $data ).'</div>';
 
-        $html .= $data['hidden'];
-        $html .= '</div>';
-
-        $html .= '<div style="'.$this->descriptionStyle.'">'.$data['description'].'</div>';
-
-        if( isset( $data['helpUrl'] ) || isset( $data['refreshUrl'] ) ){
-            $help = '';
-            if( isset( $data['helpUrl'] ) ){
-                $help = '<a href="'.$data['helpUrl'].'" target="_blank" title="Help">'.$this->helpBtnText.'</a>';
-            }
-
-            $refresh = '';
-            if( isset( $data['refreshUrl'] ) ){
-                $refresh = '<a href="'.$data['refreshUrl'].'" title="Refresh" onclick="return multicaptcha_refresh();">'.$this->refreshBtnText.'</a>';
-            }
-            $html .= '
-                <div>
-                    '.$help.$refresh.'
-                </div>';
-        }
 
 
         $html .= '</div>';
-
-        $script = '
-        <script type="text/javascript">
-
-            function multicaptcha_refresh(){
-                return false;
-            }
-        </script>
-        ';
+        $html .= $this->renderRefreshScript( $data );
+        $html .= $this->extraHtml;
 
         return $html;
+
+    }
+
+    function renderChallenge( $data ){
+        $html = '';
+        if( isset( $data['question'] ) && $data['question']['type'] == 'image' ){
+            $html .= '<img src="'.$data['question']['content'].'" style="'.$this->questionImageStyle.'"/>';
+        }elseif( isset( $data['question'] ) && $data['question']['type'] == 'text' ){
+            $html .= '<span style="'.$this->questionTextStyle.'">'.$data['question']['content'].'</span>';
+        }elseif( isset( $data['question'] ) && $data['question']['type'] == 'ascii' ){
+            $html .= '<span style="'.$this->questionAsciiStyle.'" >'.$data['question']['content'].'</span>';
+        }
+        return $html;
+    }
+
+    function renderLabel( $data ){
+        return '<span style="'.$this->labelStyle.'">'.$data['description']. '</span>';
+    }
+
+    function renderResponseField( $data ){
+        $html = '<input type="text" name="'.$data['fieldName'].
+                    '"style="'.$this->fieldStyle.'" class="'.$this->fieldClass;
+        if( isset( $data['tooltip'] ) ){
+            $html .= 'title="'.$data['tooltip'].'"';
+        }
+        $html .= '/>';
+        if( isset( $data['hidden'] ) ){
+            $html .= $data['hidden'];
+        }
+        return $html;
+    }
+
+    function renderTools( $data ){
+
+        $help = '';
+        if( isset( $data['helpUrl'] ) && strlen( $data['helpUrl'] ) > 0){
+            $help = '<a href="'.$data['helpUrl'].'" target="_blank" title="Help" class="'.$this->helpBtnClass.'">'.$this->helpBtnText.'</a>';
+        }
+
+        $refresh = '';
+        if( isset( $data['refreshUrl'] ) && strlen( $data['refreshUrl'] ) > 0 ){
+            $refresh = '<a href="'.$data['refreshUrl'].'" title="Refresh"  class="'.$this->refreshBtnClass.'" onclick="return captcha_refresh(this);">'.$this->refreshBtnText.'</a>';
+        }
+        return  $refresh.$help;
+    }
+
+    function renderRefreshScript( $data ){
+        $script = <<<"EOT"
+<script type="text/javascript">
+    function captcha_refresh( btnObj ) {
+        var AJAX = null;
+        if (window.XMLHttpRequest) {
+            AJAX=new XMLHttpRequest();
+        } else {
+            AJAX=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        if (AJAX==null) {
+            return false;
+        }
+
+        AJAX.onreadystatechange = function() {
+            if (AJAX.readyState==4) {
+                //update captcha html
+                btnObj.parentElement.parentElement.parentElement.outerHTML = this.responseText;
+                AJAX=null;
+            }
+        }
+
+        AJAX.open("GET", '{$data['refreshUrl']}', true);
+        AJAX.send(null);
+        return false;
+    }
+
+</script>
+EOT;
+        return $script;
+    }
+
+
+    /*
+     * returns the data for refreshing the captcha as json encoded array
+     */
+    function renderRefresh( $data ){
 
     }
 } 
